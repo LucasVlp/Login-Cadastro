51e44ae98dc62426673ba8e3b369a5fc8a3db373
LucasVlp <lucasvolpecosta28@gmail.com>
2025-10-27 21:21:21 -0300

    Add files via upload

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from '../views/Home'
+
 import Cadastro from "../views/Cadastro";
 import Login from "../views/Login";
 
@@ -10,7 +10,7 @@ export default function AppRoutes(){
             <Routes>
                 <Route path='/' element={<Login />} />
                 <Route path='/cadastro' element={<Cadastro />} />
-                <Route path='/home' element={<Home />} />
+                
             </Routes>
         </Router>
     )
